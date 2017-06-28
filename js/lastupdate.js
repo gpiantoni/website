dfef30e9feda0b92416ea3e02107c286546473a8
Gio on Arch <github@gpiantoni.com>
2017-06-28 12:11:33 +0200

    current version

---
+++ js/lastupdate.js
@@ -0,0 +1,7 @@
+if (Date.parse(document.lastModified) != 0) {
+  var modiDate = new Date(document.lastModified);
+  var monthName = new Array("January", "February", "March", "April", "May", 
+   "June", "July", "August", "September", "October", "November", "December");
+  document.write("Last modified: " + monthName[modiDate.getMonth()] + " ");
+  document.write(modiDate.getDate() + ", " + modiDate.getFullYear());
+  }
